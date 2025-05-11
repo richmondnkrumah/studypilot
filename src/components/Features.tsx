43163935a6fc1bd56545c28ec57f6f54e31714f2
Void <richmondnkrumah04@gmail.com>
2025-05-11 08:13:37 +0000

    fixed ui element visual look

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -8,7 +8,7 @@ const Features = (props: Props) => {
         <div className='xl:w-[1280px] flex flex-col gap-10'>
             <div className='min-w-[400px] max-w-[900px] w-full mx-auto flex flex-col gap-10'>
                 <div className='text-content flex flex-col gap-2 justify-center items-center'>
-                    <h1 className='text-4xl font-bold'>Who needs <span className='text-amber-500'>StudyPilot</span><span className='rotate-90'>?</span></h1>
+                    <h1 className='text-4xl font-bold'>Who needs <span className='text-amber-500'>StudyPilot</span><span className='inline-block rotate-20 translate-x-1 text-[42px]'>? </span></h1>
                     <p className='text-lg font-semibold text-[#707070] text-center'>Your Personal Chat GPT-but for PDFs, Read and chat with PDFs at a free, secure and fast speed</p>
                 </div>
                 <div className='flex flex-wrap gap-10 justify-center '>
