b79471fb38617b6ff1a12ab195b171cfb4a2d1e1
Void <richmondnkrumah04@gmail.com>
2025-05-11 11:15:34 +0000

    integrated faq card into faq

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import ResponsiveWrapper from './ResponsiveWrapper'
+import { CARD_DATA } from '@/constants/faq-card-contents'
+import FaqCard from './FaqCard'
 
 type Props = {}
 
 const Faq = (props: Props) => {
   return (
-    <div>Faq</div>
+   <ResponsiveWrapper>
+    <h2 className='capitalize text-5xl text-center font-bold'>frequently asked questions</h2>
+    <div className='flex flex-col gap-2'>
+      {
+        CARD_DATA.map((card,idx) => <FaqCard key={idx} title={card.title} summary={card.summary}/>)
+      }
+    </div>
+   </ResponsiveWrapper>
   )
 }
 
