3dc789c7a2f5d69a4c71cc39dcbc569b138ed692
Void <richmondnkrumah04@gmail.com>
2025-05-11 08:32:40 +0000

    modified to fit new constant data structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,20 +2,21 @@ import React from 'react'
 import Image from 'next/image'
 import { _content } from '@/constants/card-content'
 
-const Card = ({icon,title,summary,image}: _content) => {
+const Card = ({color,semititle,title,summary,image}: _content) => {
   return (
     <div className='outer rounded-2xl w-[420px] p-5 border border-gray-200'>
-        <div className='inner flex flex-col gap-4'>
-            <div className='icon'>
-                <Image src={icon} alt='card top icon' height={30} width={30}/>
-            </div>
-            <div className='text-content  flex flex-col gap-3'>
-                <h2 className='capitalize text-xl font-bold text-[#525252] '>{title}</h2>
-                <p className='text-md font-semibold text-[#707070]'>{summary}</p>
-            </div>
+        <div className='inner flex flex-col gap-3'>
             <div className='image'>
             <Image src={image} alt='card content image' width={400} className=' rounded-2xl border border-gray-100 shadow-sm '/>
             </div>
+            <div className='icon'>
+              <p className={`${color} uppercase font-semibold`}>{semititle}</p>
+            </div>
+            <div className='text-content  flex flex-col gap-1'>
+                <h2 className='capitalize text-xl font-bold text-[#525252] '>{title}</h2>
+                <p className='text-md font-semibold text-[#707070]'>{summary}</p>
+            </div>
+            
         </div>
     </div>
   )
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -8,12 +8,12 @@ const Features = (props: Props) => {
         <div className='xl:w-[1280px] flex flex-col gap-10'>
             <div className='min-w-[400px] max-w-[900px] w-full mx-auto flex flex-col gap-10'>
                 <div className='text-content flex flex-col gap-2 justify-center items-center'>
-                    <h1 className='text-4xl font-bold'>Who needs <span className='text-amber-500'>StudyPilot</span><span className='inline-block rotate-20 translate-x-1 text-[42px]'>? </span></h1>
+                    <h1 className='text-4xl font-bold'>StudyPilot in a  <span className='text-amber-500'>Nutshell</span></h1>
                     <p className='text-lg font-semibold text-[#707070] text-center'>Your Personal Chat GPT-but for PDFs, Read and chat with PDFs at a free, secure and fast speed</p>
                 </div>
                 <div className='flex flex-wrap gap-10 justify-center '>
                     {
-                        CARD_DATA.map((card,idx) => <Card key={idx} icon={card.icon} title={card.title} summary={card.summary} image={card.image} />)
+                        CARD_DATA.map((card,idx) => <Card key={idx} semititle={card.semititle} color={card.color} title={card.title} summary={card.summary} image={card.image} />)
                     }
                 </div>
             </div>
